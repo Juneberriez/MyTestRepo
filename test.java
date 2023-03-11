41c1b90bbb7ede76900831e3993adcdc965b6d6e
Saranya Fisher <juneberriez@gmail.com>
2023-03-11 08:58:52 -0500

    work has been done

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class test{
 	public static void main(String[]args){
 
-		System.out.println("changes from SDET-2");
-
+		System.out.println("changes from dev2");
+		System.out.println("more changes from dev2");
 	}
 }
